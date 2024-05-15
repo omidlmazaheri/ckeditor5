a120cc20ddba485b291fd0b51585933f6c1abcb2
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2024-05-15 14:57:49 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+ClassicEditor
+	.create( document.querySelector( '.editor' ), {
+		// Editor configuration.
+	} )
+	.then( editor => {
+		window.editor = editor;
+	} )
+	.catch( handleSampleError );
+
+function handleSampleError( error ) {
+	const issueUrl = 'https://github.com/ckeditor/ckeditor5/issues';
+
+	const message = [
+		'Oops, something went wrong!',
+		`Please, report the following error on ${ issueUrl } with the build id "pek7z5xwnh29-fv9dvfh0os5b" and the error stack trace:`
+	].join( '\n' );
+
+	console.error( message );
+	console.error( error );
+}
